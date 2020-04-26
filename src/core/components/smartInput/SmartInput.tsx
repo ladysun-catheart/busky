dd06d9986290339b1d49ee81e2b4d0b6b259e856
Lady Sun Catheart <ladysun.freedom@gmail.com>
2020-04-26 19:33:24 +0200

    [develop] simple SmartInput

--- src/core/components/smartInput/SmartInput.tsx
+++ src/core/components/smartInput/SmartInput.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const SmartInput: React.FC = () => {
-    return <div>smart input</div>
+    return (
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <div>
+                    <span>Label</span>
+                    <button>?</button>
+                </div>
+                <div>
+                    <button>close</button>
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <div style={{flex: '1'}}>
+                    <input style={{width: '100%'}} type="text" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SmartInput
